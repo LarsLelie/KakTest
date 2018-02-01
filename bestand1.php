d9c33666f03a93df3bed1546fc440c160e788f84
Lars van der Lelie <113892@student.drenthecollege.nl>
2018-02-01 11:17:57 +0100

    Nieuwe bestanden

---
+++ bestand1.php
@@ -0,0 +1,7 @@
+<html>
+  <body>
+    <h1>Dit is bestand 1</h1>
+    <h2> Bestand </h2>
+    <p> Nuttige informatie </p>
+  </body>
+</html>
---
+++ bestand2.php
@@ -0,0 +1,7 @@
+<html>
+  <body>
+    <h1>Dit is bestand 2</h1>
+    <h2> Bestand </h2>
+    <p> Nuttige informatie over bestand 2 </p>
+  </body>
+</html>
